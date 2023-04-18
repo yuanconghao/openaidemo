c73fa2666c439a896727b3a0d38058f36058fd79
yuanconghao <yuanconghao@gmail.com>
2023-04-18 15:50:18 +0800

    basic trans add time cost

--- audio/transcribe_basic.py
+++ audio/transcribe_basic.py
@@ -1,5 +1,18 @@
 import whisper
+import zhconv
+import time
 
+time1 = time.time()
 model = whisper.load_model("small")
-result = model.transcribe("qinghuaci.mp3")
-print(result["text"])
+time2 = time.time()
+print("model load cost: ", time2 - time1)
+
+result = model.transcribe("./data/svm_30.mp3")
+time3 = time.time()
+print("model audio convert: ", time3 - time2)
+
+# print the recognized text
+text = zhconv.convert(result["text"], 'zh-hans')
+print(text)
+time4 = time.time()
+print("model text convert: ", time4 - time3)

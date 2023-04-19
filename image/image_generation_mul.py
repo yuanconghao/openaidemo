c72a5ca8cc546428e87ab50d01ad6261cdbdf68a
yuanconghao <yuanconghao@gmail.com>
2023-04-19 10:38:26 +0800

    add image model

--- image/image_generation_mul.py
+++ image/image_generation_mul.py
@@ -7,16 +7,29 @@ import time
 openai.api_key = "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"
 
 # generate nums images
+prompts = [
+    "the Imperial Palace using Michelangelo Style",
+    "the Imperial Palace using Leonardo da Vinci Style",
+    "the Imperial Palace using Francisco Goya Style",
+    "the Imperial Palace using Claude Monet Style",
+    "the Imperial Palace using Pablo Picasso Style",
+    "the Imperial Palace using Jackson Pollock Style",
+    "the Imperial Palace using Wassily Kandinsky Style",
+]
+
 nums = 2
 url_list = []
-for i in range(nums):
-    response = openai.Image.create(
-        prompt="ai programmer using the Picasso style",
-        n=1,
-        size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
-    url_list.append(image_url)
-    time.sleep(10)
+
+for prompt in prompts:
+    for i in range(nums):
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+        print(image_url)
+        url_list.append(image_url)
+        time.sleep(5)
 
 print(url_list)
